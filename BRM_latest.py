1ef520774d8f38207cc7bb9dfb3dbf2b41b07c64
tlm <74514726+anonymous-editor@users.noreply.github.com>
2023-12-27 22:08:22 -0600

    Update BRM_latest.py
    Intended to match release 1.3.1, which adds partial HiDPI support.

--- BRM_latest.py
+++ BRM_latest.py
@@ -1,11 +1,12 @@
-### Developed by TLM ###
+# Dependencies: #
 
-# Dependencies #
 import tkinter as tk
 import customtkinter as ctk
 from customtkinter import filedialog, CTk, CTkLabel
+
 from PIL import Image, ImageTk
 from io import BytesIO
+import ctypes
 
 import os
 import gdown
@@ -20,13 +21,13 @@ import json # fetch file
 root = tk.Tk()
 root.withdraw()
 
-# Fetch data from url
-# TODO Fill this data
+# JSON Loading: #
+
 dataurl = "https://raw.githubusercontent.com/anonymous-editor/BRMM/main/publicmoddata.json"
 response = urllib.request.urlopen(dataurl)
 data = json.loads(response.read().decode('utf-8'))
 
-# App Frame Code
+# App Frame Code: #
 
 class MyApp(CTk):
     def __init__(self, *args, **kwargs):
@@ -35,21 +36,22 @@ class MyApp(CTk):
         self.geometry("1920x1080")
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)
+        
+        self.scrollable_frame = ctk.CTkScrollableFrame(master=self)
+        self.scrollable_frame.grid(row=0, column=0, sticky='nsew', rowspan=7, columnspan=4)
 
 app = MyApp()
 
-scrollable_frame = ctk.CTkScrollableFrame(master=app)
-scrollable_frame.grid(row=0, column=0, sticky='nsew', rowspan=7, columnspan=4)
+scrollable_frame = app.scrollable_frame
 
+# Global Theming: #
 
-# Theming #
 ctk.set_appearance_mode("System")
 ctk.set_default_color_theme("blue")
 
-# Path variables #
+# Path Variables: #
 
 def W4():
-    
     save_window = CTk()
     save_window.title("Initial Setup Status")
     save_window.geometry("550x165")
@@ -60,37 +62,28 @@ def W4():
     save_window.mainloop()
     
 if not os.path.isfile('modpath.txt'):
-    
     modpath = filedialog.askdirectory(title="Select your 'Mods' folder to continue.")
     with open('modpath.txt', 'w') as f:
-        
         f.write(modpath)
         
 else:
-    
     with open('modpath.txt', 'r') as f:
-        
         modpath = f.read()
 
 if not os.path.isfile('pakspath.txt'):
-    
     pakspath = filedialog.askdirectory(title="Select your 'Paks' folder to continue.")
     with open('pakspath.txt', 'w') as e:
-        
         e.write(pakspath)
     
     W4()
     
 else:
-    
     with open('pakspath.txt', 'r') as e:
-        
         pakspath = e.read()
-   
-# Button Controllers #
+
+# Download Operation Handlers: #
 
 def alreadyexists():
-    
     awindow = CTk()
     awindow.title("Mod Status")
     awindow.geometry("350x110")
@@ -101,7 +94,6 @@ def alreadyexists():
     awindow.mainloop()
 
 def W2():
-    
     alert_window = CTk()
     alert_window.title("Mod Status")
     alert_window.geometry("450x110")
@@ -112,9 +104,7 @@ def W2():
     alert_window.mainloop()
 
 def download_googledrive_zipfile(file_id, destination):
-
     if os.path.exists(destination):
-            
             alreadyexists()
 
             return app
@@ -124,15 +114,12 @@ def download_googledrive_zipfile(file_id, destination):
     gdown.download(url, destination, quiet=False)
          
     with zipfile.ZipFile(destination, 'r') as zip_ref:
-        
         zip_ref.extractall(os.path.dirname(destination))
 
     W2()
 
 def download_googledrive_pakfile(file_id, destination):
-
-    if os.path.exists(destination):
-            
+    if os.path.exists(destination):  
             alreadyexists()
 
             return app
@@ -143,10 +130,8 @@ def download_googledrive_pakfile(file_id, destination):
 
     W2()
 
-def download_discord_zipfile(url, filename):
-    
+def download_discord_zipfile(url, filename):   
     if os.path.exists(filename):
-            
             alreadyexists()
 
             return app
@@ -154,19 +139,15 @@ def download_discord_zipfile(url, filename):
     response = requests.get(url)
     
     with open(filename, 'wb') as file:
-        
         file.write(response.content)
 
     with zipfile.ZipFile(filename, 'r') as zip_ref:
-        
         zip_ref.extractall(os.path.dirname(filename))
 
     W2()
 
 def download_discord_pakfile(url, filename):
-
     if os.path.exists(filename):
-            
             alreadyexists()
 
             return app
@@ -179,6 +160,8 @@ def download_discord_pakfile(url, filename):
     W2()
 
     return app
+
+# Frame GUI Formatting: #
 
 class ContentFormatting:
     @staticmethod
@@ -233,10 +216,9 @@ remove_button_packing = ContentFormatting.remove_button_packing
 create_image_label = ContentFormatting.create_image_label
 create_image_label_packing = ContentFormatting.create_image_label_packing
 
-# Uninstaller #
+# Uninstaller: #
 
 def usuccessmessage():
-    
     success_window = CTk()
     success_window.title("Mod Status")
     success_window.geometry("350x110")
@@ -247,7 +229,6 @@ def usuccessmessage():
     success_window.mainloop()
     
 def notexists():
-    
     awindow = CTk()
     awindow.title("Mod Status")
     awindow.geometry("350x110")
@@ -257,16 +238,11 @@ def notexists():
       
     awindow.mainloop()
 
-# UI Stuff #
+# Main Window GUI: #
+
 title = ctk.CTkLabel(scrollable_frame, text="Available Mods:", font=("Segoe UI Semibold", 48), anchor="nw")
 title.grid(row=0, column=0, padx=15, pady=15)
 
-def W5():
-    tk.messagebox.showwarning("Warning", "Your mod type is not supported for Brick Rigs, check for any errors")
-
-def command_func():
-    remove_file(file_path1, folder_path1)
- 
 for i in range(len(data["mods"])):
     mod = data["mods"][i]
     frame_10 = create_frame(scrollable_frame)
@@ -274,21 +250,31 @@ for i in range(len(data["mods"])):
 
     textbox_16 = ctk.CTkLabel(frame_10, text=mod["name"], **mod_title_font())
     textbox_16.pack(anchor="center", padx=10, pady=10)
-    
-    # TODO: replace with better solution
-    response = requests.get(mod["image"]) # too slow
-    image_path = Image.open(BytesIO(response.content))
-    image_path = image_path.resize((480, 270))
 
-    photo = ImageTk.PhotoImage(image_path, master=app)
+    # Image Scaling Functionality: #
+
+    def get_scaling_factor():
+        user32 = ctypes.windll.user32
+        dpi = user32.GetDpiForSystem()
+        return dpi / 96
+
+    scaling_factor = get_scaling_factor()
+
+    response = requests.get(mod["image"])
+    image = Image.open(BytesIO(response.content))
+    resized_image = image.resize((int(480 * scaling_factor), int(270 * scaling_factor)))
+
+    photo = ImageTk.PhotoImage(resized_image, master=app)
 
     label = ctk.CTkLabel(frame_10, image=photo, text="")
-    label.image = photo  # keep a reference!
+    label.image = photo
     label.pack(**create_image_label_packing('center'))
 
     textbox_17 = ctk.CTkLabel(frame_10, font=("Segoe UI", 16), text=mod["description"]+"\n\nSize: "+mod["size"]+"\nAuthor: "+mod["author"], wraplength=500)
     textbox_17.pack(anchor="center", padx=30, pady=15)
 
+    # Download Handlers: #
+    
     if mod["installType"] == "gd": # gd = Google Drive
         destination = os.path.join(modpath, mod["installpath"])
         button = ctk.CTkButton(frame_10, command=lambda file_id=mod["install"], destination=f'{modpath}{mod["installpath"]}': download_googledrive_zipfile(file_id, destination), **install_button())
@@ -309,6 +295,11 @@ for i in range(len(data["mods"])):
         button = ctk.CTkButton(frame_10, command=lambda file_id=mod["install"], destination=f'{pakspath}{mod["installpath"]}': download_discord_pakfile(file_id, destination), **install_button())
         button.pack(**install_button_packing())
 
+    # File Removal Managers: #
+    
+    def command_func():
+        remove_file(file_path1, folder_path1)
+    
     if mod["deinstallType"] == "zip":
         def remove_file(file_path1, folder_path1):
             if not os.path.exists(file_path1):
